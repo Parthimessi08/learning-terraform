847de9156fad6a548b181e93afdc3e4ef5b9f147
Parthiban D <66234642+Parthimessi08@users.noreply.github.com>
2024-12-23 17:49:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
