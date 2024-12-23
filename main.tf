0ce64d906ca70aff5e04abc055d8d2dd7ba445e5
Parthiban D <66234642+Parthimessi08@users.noreply.github.com>
2024-12-23 15:45:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "blog" {
 
 resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
-  from_port   = 80
-  to_port     = 80
+  from_port   = 81
+  to_port     = 81
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
